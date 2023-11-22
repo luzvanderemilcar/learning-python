d2466bb052a4c4149c026ec541fd43bde8b2b75e
emiltog@gmail <emiltog@gmail>
2023-11-22 11:24:16 -0500

    update weight_converter.py

--- calculator/weight_converter.py
+++ calculator/weight_converter.py
@@ -6,17 +6,19 @@ def quit_process(action):
         
 lb_to_kg_factor = 0.45359237
 kg_to_lb_factor = 2.2046226218
+
+# control the success of Weight input
 weight_is_fine = False
 while weight_is_fine is False:
     print("Type 'quit' to exit")
     weight = input("Your weight : ").strip()
     quit_process(weight)
-    weight_unit = input("Enter L (lbs) or K (kgs)").strip()
+    weight_unit = input("Enter L (lb) or K (kg)").strip()
     quit_process(weight_unit)
     
     while weight_unit.upper() != "L" and weight_unit.upper() != "K":
         print("Enter a valid weight unit")
-        weight_unit = input("Enter L (lbs) or K (kgs)").strip()
+        weight_unit = input("Enter L (lb) or K (kg)").strip()
         quit_process(weight_unit)
     if weight_unit.upper() == "L" or weight_unit.upper() == "K":
         if weight_unit.upper() == "L":
