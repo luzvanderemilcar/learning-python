5c71d5ec5980a81042239c131378ad60bcdb6a23
emiltog@gmail <emiltog@gmail>
2023-11-23 12:51:42 -0500

    update file_reader.py - add descriptive notes and self-explanatory words

--- file-reading/file_reader.py
+++ file-reading/file_reader.py
@@ -1,35 +1,36 @@
 filename = input("Enter file name : ").strip()
-is_Fine = False
+filename_Is_Fine = False
 lookup_characters = "@gmail.com"
-default_text  = '''
+main_text  = '''
         From: example@gmail.com
         To: example2@gmail.com
         Cc: example3@gmail.com
        '''
-characters = input("Enter the your search:_ ")
+characters = input("Enter your search:_ ")
 if len(characters.strip()) > 0:
     lookup_characters = characters
 try:
     file_handle = open(filename)
-    is_Fine = True
+    filename_Is_Fine = True
 except:
     print(f"File {filename} Can Not Be Opened")
-    text_content = input("Paste here your text content:_ ")
+    text_content = input("Paste your text content:_ ")
     if len(text_content.strip()) > 0:
-        default_text = text_content
+        main_text = text_content
     # quit()
     
+    # while loop processing the whole text
     count=0
     index=0
-    while index < len(default_text):
-        if lookup_characters not in default_text[index:]:
+    while index < len(main_text):
+        if lookup_characters not in main_text[index:]:
             break
-        index = default_text.find(lookup_characters, index) + len(lookup_characters)
+        index = main_text.find(lookup_characters, index) + len(lookup_characters)
         count += 1
-    print(f"'{lookup_characters}' is {count} times referenced in\n{default_text}")
+    print(f"'{lookup_characters}' is {count} times referenced in\n{main_text}")
     
-# For loop for processing
-if is_Fine:
+# For loop processing, line by line
+if filename_Is_Fine:
     count = 0
     for line in file_handle:
         if lookup_characters not in line:
